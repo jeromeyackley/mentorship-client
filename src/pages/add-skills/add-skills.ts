2ea98c172dd1ce73de379a58d1953baa4a8a7835
kevinmontanez <kmontanez@multiservice.com>
2018-02-13 12:38:42 -0600

    skills search

--- src/pages/add-skills/add-skills.ts
+++ src/pages/add-skills/add-skills.ts
@@ -15,13 +15,40 @@ import {UserProvider} from "../../providers/user/user";
   templateUrl: 'add-skills.html',
 })
 export class AddSkillsPage {
-  searchString = '';
-  skills = ['moon walking', 'bull riding', 'ice skating', 'electric triangle'];
+  skills = [];
+  _skills = [];
   constructor(public navCtrl: NavController, public navParams: NavParams, public userProvider: UserProvider) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AddSkillsPage');
+    this.getSkills();
+    this.initializeItems();
   }
+
+  getSearchResults(ev: any) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the searchbar
+    let val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.skills = this._skills.filter((skill) => {
+        return (skill.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
+
+  initializeItems() {
+    this.skills = this._skills;
+  }
+
+
+  getSkills(){
+    this._skills = ['moon walking', 'bull riding', 'ice skating', 'electric triangle'];
+  }
+
 
 }
