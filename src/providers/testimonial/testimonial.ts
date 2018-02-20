47ff2d211091cfa9948ae7e71d3c2abaa6a5b1d1
Jerome D. Yackley <jyackley@msts.com>
2018-02-20 15:38:57 -0600

    Added testimonials endpoint fix that was missing from last commit.

--- src/providers/testimonial/testimonial.ts
+++ src/providers/testimonial/testimonial.ts
@@ -14,10 +14,10 @@ export class TestimonialProvider {
   }
 
   addTestimonial(body, sessionToken) {
-    return this.http.post('https://mentr.msts.com/api', body, {headers: new HttpHeaders({"Content-Type":"application/json", "Authorization":sessionToken})})
+    return this.http.post('https://mentr.msts.com/api/testimonials', body, {headers: new HttpHeaders({"Content-Type":"application/json", "Authorization":sessionToken})})
   }
 
   getTestimonialsForUser(user_id, sessionToken) {
-    return this.http.get('https://mentr.msts.com/api/for_user/' + user_id, {headers: new HttpHeaders({"Content-Type":"application/json", "Authorization":sessionToken})})
+    return this.http.get('https://mentr.msts.com/api/testimonials/for_user/' + user_id, {headers: new HttpHeaders({"Content-Type":"application/json", "Authorization":sessionToken})})
   }
 }
